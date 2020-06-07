e392d67acb3db158e114a8ddc7ccf486a27f58fd
seepiol <seepiol@protonmail.com>
2020-06-07 22:38:43 +0200

    different instances

--- main.py
+++ main.py
@@ -8,9 +8,25 @@ print(" | ||  \| |\ \ / / | || | | | | | | | |   | |    | |")
 print(" | || |\  | \ V /  | || |_| | | |_| | |___| |___ | | ")
 print("|___|_| \_|  \_/  |___|____/___\___(_)____|_____|___|")
 
+print("""
+Select an instance:
+1) invidio.us
+2) invidious.snopyta.org
+3) yewtu.be
+
+""")
+instance = input("Select an instance: ")
+if instance == "1":
+    instance = "https://invidio.us"
+elif instance == "2":
+    instance = "https://invidious.snopyta.org"
+elif instance == "3":
+    instance = "https://yewtu.be"
+
 search_query = input("Search something > ")
 
-url = "https://invidio.us/search?q="+search_query
+url = instance+"/search?q="+search_query
+
 results_list = []
 
 page = BeautifulSoup(get(url).text, features="html5lib")
@@ -21,7 +37,7 @@ for link in page.find_all('a'):
         if link.string != None:
             print(f"[{i}]=========================================================================================================")
             print(link.string)
-            video_link = "https://invidio.us"+link.get("href")
+            video_link = instance+link.get("href")
             print(video_link)
             results_list.append(video_link)
             i+=1
@@ -32,5 +48,3 @@ selected_video = int(input("Insert the number of the video you want to watch: ")
 while selected_video == None or selected_video < 1 or selected_video >= len(results_list):
     selected_video = int(input("[UNVALID DATA] Insert the number of the video you want to watch: "))
 webbrowser.open(results_list[selected_video-1], new=2)
-
-
